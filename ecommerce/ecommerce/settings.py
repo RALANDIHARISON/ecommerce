e39658e4fdde4383f7feb89862544aa2ea05b476
Lalaina <nolalaina.ralandiharison@stud.unifi.it>
2025-06-27 08:08:16 +0200

    Add .railway.app to ALLOWED_HOSTS

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ecommerce-production-8787.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ecommerce-production-8787.up.railway.app', '.railway.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
